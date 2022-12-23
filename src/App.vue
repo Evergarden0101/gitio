cbe34d28458b93db5aae290de8b3cd24975f5484
nanashuka <164695704@qq.com>
2022-12-23 01:28:19 +0100

    navi

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,57 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!-- <router-view/> -->
+
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect" style="margin-bottom:50px">
+      <el-menu-item index="1" style="font-size:16px;width: 140px;">My Bio</el-menu-item>
+      <el-menu-item index="2" style="font-size:16px;width: 140px;">Only For 🌼盈🤫</el-menu-item>
+      <!-- <el-submenu index="2">
+        <template slot="title">我的工作台</template>
+        <el-menu-item index="2-1">选项1</el-menu-item>
+        <el-menu-item index="2-2">选项2</el-menu-item>
+        <el-menu-item index="2-3">选项3</el-menu-item>
+        <el-submenu index="2-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="2-4-1">选项1</el-menu-item>
+          <el-menu-item index="2-4-2">选项2</el-menu-item>
+          <el-menu-item index="2-4-3">选项3</el-menu-item>
+        </el-submenu>
+      </el-submenu> -->
+      <!-- <el-menu-item index="3" disabled>消息中心</el-menu-item>
+      <el-menu-item index="4"><a href=" " target="_blank">订单管理</a></el-menu-item> -->
+    </el-menu>
+
+    <el-row v-if="activeIndex==1">
+      <h1>I CAN DO ALL THINGS!</h1>
+    </el-row>
+    <el-row v-if="activeIndex==2">
+      <el-col :offset="4" :span="16">
+        <template>
+          <el-carousel trigger="click" :interval="4000" type="card" height="250px" width="800px">
+            <el-carousel-item v-for="item in 6" :key="item">
+              <h3 class="medium">{{ item }}</h3>
+            </el-carousel-item>
+          </el-carousel>
+        </template>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      activeIndex: '1',
+    }
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      this.activeIndex = key;
+      console.log(key, keyPath);
+    }
+  }
 }
 </script>
 
@@ -18,6 +62,20 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 200px;
+    margin: 0;
+  }
+  
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n+1) {
+  background-color: #d3dce6;
 }
 </style>
